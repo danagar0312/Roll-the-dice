3fd70eb2ed7ced7be1b4bfd32c057147d62f6655
Awasthi <dawasthi@3inmdawasthi2.ad.here.com>
2019-08-29 17:05:16 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var scores,activeScore,roundScore;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+
+document.querySelector('.dice').style.display = 'none';
+
+
+document.querySelector('.btn-roll').addEventListener('click',function() {
+    var dice = Math.floor(Math.random() * 6) + 1;
+    var diceDOM = document.querySelector('.dice');  
+    diceDOM.style.display = 'block';
+    diceDOM.src = "dice-" + dice + '.png';
+});
